ffcea33d6b700a56a48250f856ef15529cbea012
Алексей Семендеев <ken123@bk.ru>
2022-12-28 09:09:24 +0900

    new

--- ModelPaintJAVA/src/utility/Model.java
+++ ModelPaintJAVA/src/utility/Model.java
@@ -30,32 +30,10 @@ public class Model {
         return faces;
     }
 
-    public static class Material {
-
-        @Override
-        public String toString() {
-            return "Material{" +
-                    "specularCoefficient=" + specularCoefficient +
-                    ", ambientColour=" + ambientColour +
-                    ", diffuseColour=" + diffuseColour +
-                    ", specularColour=" + specularColour +
-                    '}';
-        }
-
-
-        public float specularCoefficient = 100;
-        public float[] ambientColour = {0.2f, 0.2f, 0.2f};
-        public float[] diffuseColour = {0.3f, 1, 1};
-        public float[] specularColour = {1, 1, 1};
-    }
-
 
     public static class Face {
-
         private final int[] vertexIndices = {-1, -1, -1};
         private final int[] normalIndices = {-1, -1, -1};
-        private final int[] textureCoordinateIndices = {-1, -1, -1};
-        private Material material;
 
         public int[] getVertexIndices() {
             return vertexIndices;
@@ -78,19 +56,6 @@ public class Model {
             this.normalIndices[0] = normalIndices[0];
             this.normalIndices[1] = normalIndices[1];
             this.normalIndices[2] = normalIndices[2];
-        }
-
-        public Face(int[] vertexIndices, int[] normalIndices, int[] textureCoordinateIndices, Material material) {
-            this.vertexIndices[0] = vertexIndices[0];
-            this.vertexIndices[1] = vertexIndices[1];
-            this.vertexIndices[2] = vertexIndices[2];
-            this.textureCoordinateIndices[0] = textureCoordinateIndices[0];
-            this.textureCoordinateIndices[1] = textureCoordinateIndices[1];
-            this.textureCoordinateIndices[2] = textureCoordinateIndices[2];
-            this.normalIndices[0] = normalIndices[0];
-            this.normalIndices[1] = normalIndices[1];
-            this.normalIndices[2] = normalIndices[2];
-            this.material = material;
         }
     }
 }
